a3ede5a8731484319d4fc2691832b8ba54e53086
if-you-love-me-forever-52lin1314 <126077005+if-you-love-me-forever-52lin1314@users.noreply.github.com>
2024-12-17 12:24:58 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -38,17 +38,7 @@
     // 控制台输出日志信息（可选）
     console.log('夜间模式已开启（全天）');
 })();
-	//夜间模式切换
-	function switchNightMode(){
-		var night = document.cookie.replace(/(?:(?:^|.*;\s*)night\s*\=\s*([^;]*).*$)|^.*$/, "$1") || '0';
-		if(night == '0'){
-			document.body.classList.add('night');
-			document.cookie = "night=1;path=/"
-			console.log('夜间模式开启');
-		}else{
-			document.body.classList.remove('night');
-			document.cookie = "night=0;path=/"
-			console.log('夜间模式关闭');
+
 		}
 	}
 	//全屏切换
